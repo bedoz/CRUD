92dbab59d025aacb82d8e199046a261954c22d0f
Alex Bedendo <alex.bedendo@mapostudio.com>
2018-07-24 10:28:24 +0200

    Translation infinite loop fixed
    Took 1 minute

--- src/ModelTraits/DimsavTranslatable/HasTranslations.php
+++ src/ModelTraits/DimsavTranslatable/HasTranslations.php
@@ -39,6 +39,29 @@ trait HasTranslations
         return $translation;
     }
     
+    public function getAttribute($key) {
+        list($attribute, $locale) = $this->getAttributeAndLocale($key);
+        
+        if ($this->isTranslationAttribute($attribute)) {
+            if ($this->getTranslation($locale) === null) {
+                return ""; //$this->getAttributeValue($attribute);
+            }
+
+            // If the given $attribute has a mutator, we push it to $attributes and then call getAttributeValue
+            // on it. This way, we can use Eloquent's checking for Mutation, type casting, and
+            // Date fields.
+            if ($this->hasGetMutator($attribute)) {
+                $this->attributes[$attribute] = $this->getAttributeOrFallback($locale, $attribute);
+
+                return $this->getAttributeValue($attribute);
+            }
+
+            return $this->getAttributeOrFallback($locale, $attribute);
+        }
+
+        return parent::getAttribute($key);
+    }
+    
     /*public function getAttributes(){
         return $this->translatedAttributes;
     }*/
