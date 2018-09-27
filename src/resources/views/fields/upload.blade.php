1ee85b469e7230500120d2858ffa1ec2c1f2333d
Alex Bedendo <alex.bedendo@mapostudio.com>
2018-09-27 15:29:58 +0200

    Upload have no hidden input so it cannot be saved in any way. Now it have a hidden input and it is always present (not removed anymore on change)
    Took 2 minutes

--- src/resources/views/fields/upload.blade.php
+++ src/resources/views/fields/upload.blade.php
@@ -3,7 +3,7 @@
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
 
-	{{-- Show the file name and a "Clear" button on EDIT form. --}}
+    {{-- Show the file name and a "Clear" button on EDIT form. --}}
     @if (!empty($field['value']))
     <div class="well well-sm">
         @if (isset($field['disk']))
@@ -13,20 +13,20 @@
         @endif
             {{ $field['value'] }}
         </a>
-    	<a id="{{ $field['name'] }}_file_clear_button" href="#" class="btn btn-default btn-xs pull-right" title="Clear file"><i class="fa fa-remove"></i></a>
-    	<div class="clearfix"></div>
+        <a id="{{ $field['name'] }}_file_clear_button" href="#" class="btn btn-default btn-xs pull-right" title="Clear file"><i class="fa fa-remove"></i></a>
+        <div class="clearfix"></div>
     </div>
     @endif
 
-	{{-- Show the file picker on CREATE form. --}}
-	<input
+    {{-- Show the file picker on CREATE form. --}}
+    <input
         type="file"
         id="{{ $field['name'] }}_file_input"
         name="{{ $field['name'] }}"
         value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
         @include('crud::inc.field_attributes', ['default_class' =>  isset($field['value']) && $field['value']!=null?'form-control hidden':'form-control'])
     >
-
+    <input type="hidden" name="{{ $field['name'] }}">
     {{-- HINT --}}
     @if (isset($field['hint']))
         <p class="help-block">{!! $field['hint'] !!}</p>
@@ -36,24 +36,24 @@
 {{-- FIELD EXTRA JS --}}
 {{-- push things in the after_scripts section --}}
 
-    @push('crud_fields_scripts')
-        <!-- no scripts -->
-        <script>
-	        $("#{{ $field['name'] }}_file_clear_button").click(function(e) {
-	        	e.preventDefault();
-	        	$(this).parent().addClass('hidden');
+@push('crud_fields_scripts')
+    <!-- no scripts -->
+    <script>
+        $("#{{ $field['name'] }}_file_clear_button").click(function(e) {
+            e.preventDefault();
+            $(this).parent().addClass('hidden');
 
-	        	var input = $("#{{ $field['name'] }}_file_input");
-	        	input.removeClass('hidden');
-	        	input.attr("value", "").replaceWith(input.clone(true));
-	        	// add a hidden input with the same name, so that the setXAttribute method is triggered
-	        	$("<input type='hidden' name='{{ $field['name'] }}' value=''>").insertAfter("#{{ $field['name'] }}_file_input");
-	        });
+            var input = $("#{{ $field['name'] }}_file_input");
+            input.removeClass('hidden');
+            input.attr("value", "").replaceWith(input.clone(true));
+            // add a hidden input with the same name, so that the setXAttribute method is triggered
+            $("<input type='hidden' name='{{ $field['name'] }}' value=''>").insertAfter("#{{ $field['name'] }}_file_input");
+        });
 
-	        $("#{{ $field['name'] }}_file_input").change(function() {
-	        	console.log($(this).val());
-	        	// remove the hidden input, so that the setXAttribute method is no longer triggered
-	        	$(this).next("input[type=hidden]").remove();
-	        });
-        </script>
-    @endpush
+        /*$("#{{ $field['name'] }}_file_input").change(function() {
+            console.log($(this).val());
+            // remove the hidden input, so that the setXAttribute method is no longer triggered
+            $(this).next("input[type=hidden]").remove();
+        });*/
+    </script>
+@endpush
